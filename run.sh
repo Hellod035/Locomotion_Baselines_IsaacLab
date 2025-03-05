f39bf613a3e7116f507cea6890ad982866fbf056
Hellod035 <hellod035@163.com>
2025-03-05 17:57:58 +0800

    fix runbash

--- run.sh
+++ run.sh
@@ -1,33 +1,19 @@
-### Locomotion H1 ###
-
 conda activate unitree_lab
 export WANDB_USERNAME='xxxxx'
-python scripts/rsl_rl/train.py --task H1-Flat --headless
 
-conda activate unitree_lab
+### Locomotion H1 ###
+python scripts/rsl_rl/train.py --task H1-Flat --headless
 python scripts/rsl_rl/play.py --task H1-Flat-Play
 
-conda activate unitree_lab
-export WANDB_USERNAME='xxxxx'
 python scripts/rsl_rl/train.py --task H1-Rough --headless
-
-conda activate unitree_lab
 python scripts/rsl_rl/play.py --task H1-Rough-Play
 
 
 ### Locomotion G1 ###
 
-conda activate unitree_lab
-export WANDB_USERNAME='xxxxx'
 python scripts/rsl_rl/train.py --task G1-Flat --headless
-
-conda activate unitree_lab
 python scripts/rsl_rl/play.py --task G1-Flat-Play
 
-conda activate unitree_lab
-export WANDB_USERNAME='xxxxx'
 python scripts/rsl_rl/train.py --task G1-Rough --headless
-
-conda activate unitree_lab
 python scripts/rsl_rl/play.py --task G1-Rough-Play
 
